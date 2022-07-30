e7688bccd301a75c3cf5bb48fbae398acd969ec8
Alina Costa <alinacosta@Alinas-MacBook-Pro-2.local>
2022-07-30 17:04:34 -0400

    added eventListener to save buttons

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,3 +1,5 @@
+var saveButtonEl = Array.from(document.getElementsByClassName("saveBtn"));
+
 // get and display current date with moment.js
 var currentDayEl = document.getElementById("currentDay");
 function displayDate() {
@@ -5,3 +7,21 @@ function displayDate() {
   currentDayEl.textContent = currentDate;
 }
 displayDate();
+
+// save events in local stoarge so it remains when the page is refreshed
+// use target to take textContent from the input where the save button was clicked -
+// and update that with whatever was in local storage
+
+function saveTasks() {
+  console.log("what the heck");
+  var taskInput = document.getElementsByTagName("input").textContent;
+  // localStorage.setItem("savedTask", JSON.stringify())
+}
+
+saveButtonEl.forEach((btn) => {
+  btn.addEventListener("click", saveTasks);
+});
+
+// taskInput.forEach((input) => {
+//   input.addEventListener("input", saveTasks);
+// });
