57faf205e5244dea91359d44a8d97dce0a546b70
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-08-01 18:20:39 -0400

    create local-storage branch

---
+++ Develop/assets/script.js
@@ -0,0 +1,26 @@
+var saveButtonEl = Array.from(document.getElementsByClassName("saveBtn"));
+var timeBlockEl = document.querySelector(".time-block");
+var userInput = "";
+
+// get and display current date with moment.js
+var currentDayEl = document.getElementById("currentDay");
+function displayDate() {
+  var currentDate = moment().format("MMMM Do YYYY");
+  currentDayEl.textContent = currentDate;
+}
+displayDate();
+
+function saveTasks(event) {
+  var target = $(event.target);
+  var taskInput = $(target).prev().attr("id");
+  userInput = $(this).prev().val();
+  console.log(userInput);
+}
+
+saveButtonEl.forEach((btn) => {
+  btn.addEventListener("click", saveTasks);
+});
+
+// save descriptions in local stoarge so it remains when the page is refreshed
+// get input from where the save button was clicked -
+// and update that into local storage
