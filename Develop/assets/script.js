b677ac8f88c30938211b334a9705e5465ec20511
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-08-01 22:12:21 -0400

    get id of array objects

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,6 +1,8 @@
 var saveButtonEl = Array.from(document.getElementsByClassName("saveBtn"));
 var timeBlockEl = document.querySelector(".time-block");
 var userInput = "";
+var inputEl = $(".description");
+var inputElArr = Array.from(inputEl);
 
 // get and display current date with moment.js
 var currentDayEl = document.getElementById("currentDay");
@@ -21,6 +23,30 @@ saveButtonEl.forEach((btn) => {
   btn.addEventListener("click", saveTasks);
 });
 
-// save descriptions in local stoarge so it remains when the page is refreshed
-// get input from where the save button was clicked -
-// and update that into local storage
+// change time block color based on past/present/future
+//for loop?
+// while inputEl(id) + 12 > current-hour, addClass(".future")
+// while id attr + 12 == current-hour, addClass(".present")
+// while id attr + 12 < current-hour, addClass(".past")
+
+// get current time from moment.js
+
+// for each inputEl
+
+//function for relative time color
+function currentHour() {
+  var currentTime = moment().hour();
+  for (i = 0; i < inputElArr.length; i++) {
+    var inputElId = inputElArr[i].getAttribute("id");
+    while (inputElId + 12 > currentTime) {
+      $(inputElArr[i]).addClass("past");
+    }
+  }
+}
+currentHour();
+//   while () {
+
+//   }
+// }
+
+// save userInput in local stoarge so it remains when the page is refreshed
